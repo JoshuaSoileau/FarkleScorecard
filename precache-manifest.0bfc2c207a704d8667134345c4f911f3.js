e828adadf0537df31f33c2786b011fd05177a450
Joshua Soileau <joshua.soileau@gmail.com>
2019-08-20 21:55:06 -0400

    Updates

---
+++ precache-manifest.0bfc2c207a704d8667134345c4f911f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013d4eab997fb5261951f7d49f398538",
+    "url": "/FarkleScorecard/index.html"
+  },
+  {
+    "revision": "60f14195ccf1dbd683f8",
+    "url": "/FarkleScorecard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "613463702f7d3c81cf50",
+    "url": "/FarkleScorecard/static/js/2.27ad7d11.chunk.js"
+  },
+  {
+    "revision": "60f14195ccf1dbd683f8",
+    "url": "/FarkleScorecard/static/js/main.7888a778.chunk.js"
+  },
+  {
+    "revision": "e0c58aec5ba3c693e73b",
+    "url": "/FarkleScorecard/static/js/runtime~main.8ad60577.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FarkleScorecard/static/media/logo.5d5d9eef.svg"
+  }
+]);
